154a06687ebb7e3f47158ef4f37b92a363d0bb1c
Ghadeer-Moh <139185332+Ghadeer-Moh@users.noreply.github.com>
2023-09-11 10:08:18 +0300

    Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firstproject';
+  title = 'Welcome to my First App ^-^';
+  length =100;
+  b = 50;
 }
